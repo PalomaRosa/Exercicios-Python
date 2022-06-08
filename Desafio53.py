22689b7cab6ed6229eee07f20e72c071c189ffa4
Paloma da Rosa Velho <86445450+PalomaRosa@users.noreply.github.com>
2022-06-07 22:51:11 -0300

    Update Desafio53.py

--- Desafio53.py
+++ Desafio53.py
@@ -1,7 +1,7 @@
 # Analisador completo
-
 # Programa que irá ler o nome, idade e sexo de 4 pessoas. E no final do programa, irá exibir
 # a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+
 somapess = 0
 somaida = 0
 total = 0
